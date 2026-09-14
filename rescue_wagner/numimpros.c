6fac9851f691442a956c07480812ec2cf7e52f7d
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 15:12:29 -0300

    Update numimpros.c

--- rescue_wagner/numimpros.c
+++ rescue_wagner/numimpros.c
@@ -1,1 +1,9 @@
-
+int numimpros( int src , int tgt , char * grpwasok ) 
+{
+    /** 
+      if any positively constrained group is non-mono, return 0; 
+      if more negatively constrained groups than in the original 
+         tree (stored in grpwasok) are now non-mono, return number
+         of additional groups made non-mono
+    */
+}    
