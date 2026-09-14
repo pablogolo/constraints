ae3cd417fe0fa1b1895d89a48c6caf484dede915
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 14:45:59 -0300

    Add fill_root_list function implementation
    Implement fill_root_list function to populate a list based on clip value and node descriptors.

--- swapping_unconstrained/fill_root_list.c
+++ swapping_unconstrained/fill_root_list.c
@@ -1,1 +1,20 @@
-
+int fill_root_list ( int clip , int * list ) {
+  int * todo , * done , aux ;  
+  todo = list ;
+  if ( clip >= nt ) {
+    * todo = lefdes [ clip ] ;
+    aux = rigdes [ clip ] ;
+    if ( aux >= nt ) 
+      * ++ todo = aux ;
+   }
+  else * todo = clip ;     
+  done = todo ++ ;
+  while ( done < todo ) {
+    at = * done ++ ; 
+    if ( at >= nt ) { 
+      * todo ++ = lefdes [ at ] ; 
+      * todo ++ = rigdes [ at ] ;
+     }
+   }
+  return todo - list ; 
+}   
