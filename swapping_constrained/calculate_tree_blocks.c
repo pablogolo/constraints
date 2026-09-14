7889ccefeb73e66d10470dccbf0f6390e7f06df1
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 14:47:26 -0300

    Add calculate_tree_blocks function
    Implement the calculate_tree_blocks function to manage tree block calculations based on node sizes and references.

--- swapping_constrained/calculate_tree_blocks.c
+++ swapping_constrained/calculate_tree_blocks.c
@@ -1,1 +1,20 @@
-
+void calculate_tree_blocks ( char * blocks ) {
+  int i , a , b , csz ;
+  int lim = ref_tree [ nt ] ; 
+  for ( i = nt - 1 ; ++ i < nodz ; ) blocks [ i ] = nodlist [ i ] = 0 ;
+  nodlist[ nt ] = 1 ;
+  fill_nodsizes( grpsiz ) ; // trivial
+  for ( i = nt ; i ++ < lim ; ) {
+    b = ref_tree [ a = constlast [ i ] ] ;
+    a = ancestor [ a ] ;
+    while ( !nodlist [ b ] ) {
+      csz = const_nodsiz [ b ] ; 
+      nodlist[ b ] = 1 ;
+      b = ref_tree [ b ] ; 
+      while ( grpsiz [ a ] != csz && a != nt ) a = ancestor [ a ] ;
+      blocks [ a ] = 1 ;
+     }
+   }
+  blocks [ nt ] = 1 ; 
+  return ; 
+}
