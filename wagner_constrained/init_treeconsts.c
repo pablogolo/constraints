479923ca6f6ab93c22c0e85d583ff67b10f015ee
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 14:57:36 -0300

    Implement init_treeconsts function for tree initialization

--- wagner_constrained/init_treeconsts.c
+++ wagner_constrained/init_treeconsts.c
@@ -1,1 +1,39 @@
-
+void init_treeconsts ( void )
+{
+  int a ;
+  int treeconstnum = ref_tree [ nt ] ; 
+  for( a = 0 ; a < treeconstnum ; ++ a ) 
+    const_nodsiz[ a ] = constlast [ a ] = isblocked [ a ] = 0 ; 
+  const_nodsiz[ nt ] = 1 ;
+  a = ref_tree [ lefdes [ nt + 1 ]] ;
+  while ( a && a < treeconstnum ) {
+    ++ const_nodsiz[ a ] ;
+    constlast [ a ] = lefdes [ nt + 1 ];
+    a = ref_tree [ a ] ;
+   } 
+  a = ref_tree [ rigdes [ nt + 1 ]] ; 
+  while ( a && a < treeconstnum ) {
+    ++ const_nodsiz[ a ] ;
+    constlast [ a ] = rigdes [ nt + 1 ];
+    a = ref_tree [ a ] ;
+   }
+  a = ref_tree [ lefdes [ nt ] ] ; 
+  while ( a && const_nodsiz[ a ] < 2 && a != nt ) a = ref_tree [ a ] ;
+  grpsiz[ nt + 1 ] = grpsiz[ nt ] = 0 ;
+  if ( ref_tree [ lef [ nt + 1 ] ] ) {
+    grpsiz[ nt + 1 ] ++ ;
+    grpsiz[ nt ] ++ ;
+    if ( a && ref_tree [ lefdes [ nt + 1 ]] )
+      constlast [ a ] = lefdes [ nt + 1 ] ;
+   }
+  if ( ref_tree [ rig [ nt + 1 ] ] ) {
+    grpsiz[ nt + 1 ] ++ ;
+    grpsiz[ nt ] ++ ;
+    if ( a && ref_tree [ rigdes [ nt + 1 ]] )
+      constlast [ a ] = rigdes [ nt + 1 ] ;
+   }
+  if ( !constlast [ nt ] ) constlast [ nt ] = sis [ 0 ] ; 
+  if ( ref_tree [ 0 ] ) 
+    grpsiz[ nt ] ++ ;
+  return ;
+}
