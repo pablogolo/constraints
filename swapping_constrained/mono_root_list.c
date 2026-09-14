a755fcc62cbf9b8209506e973e96b11b62a339a8
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 14:49:24 -0300

    Add mono_root_list function for list processing
    Implement mono_root_list function to process a list based on clipping and blocking conditions.

--- swapping_constrained/mono_root_list.c
+++ swapping_constrained/mono_root_list.c
@@ -1,1 +1,22 @@
-
+int mono_root_list( int clip , int * list ) {
+  int * todo , * done , aux ; 
+  todo = list ;
+  if ( clip >= nt ) {
+    * todo = lefdes [ clip ] ;
+    aux = rigdes [ clip ] ; 
+    if ( aux >= nt && !isblocked [ aux ] ) {
+       * ++ todo = lefdes [ aux ] ; 
+       * ++ todo = rigdes [ aux ] ;
+      }
+    }
+  else * todo = clip ;         
+  done = todo ++ ;
+  while ( done < todo ) {
+    at = * done ++ ; 
+    if ( at >= nt && !isblocked [ at ] ) { 
+      * todo ++ = lefdes [ at ] ; 
+      * todo ++ = rigdes [ at ] ;
+     }
+   }
+  return todo - list ; 
+}
