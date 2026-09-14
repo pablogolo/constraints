af13b1c8c41d74ed2dc7a415f163a0e9fa20c6c6
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 14:48:23 -0300

    Add mono_insert_list function for constrained insertion
    Implement mono_insert_list function to manage constrained insertion.

--- swapping_constrained/mono_insert_list.c
+++ swapping_constrained/mono_insert_list.c
@@ -1,1 +1,17 @@
-
+int mono_insert_list ( int from , int * list ) {
+  int * todo , * done , isfirst ;  
+  while ( !isblocked [ from ] )
+    from = ancestor [ from ] ; // more groups constrained, stops earlier!
+  *  ( todo = list ) = from ;
+  done = todo ++ ;
+  isfirst = 1 ; 
+  while ( done < todo ) {
+    at = * done ++ ; 
+    if ( at >= nt && ( !isblocked [ at ] || isfirst ) ) { 
+      * todo ++ = lefdes [ at ] ; 
+      * todo ++ = rigdes [ at ] ;
+     }
+    isfirst = 0 ;
+   }
+  return todo - list ; 
+}
