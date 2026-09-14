68bcf8b0b88adb7121bff73b4e40f16bf09616c0
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 14:50:19 -0300

    Add no_moves_possible function
    Implement no_moves_possible function to check move validity.

--- swapping_constrained/no_moves_possible.c
+++ swapping_constrained/no_moves_possible.c
@@ -1,1 +1,9 @@
-
+int no_moves_possible( int clip , int dad ) {
+  if ( isblocked [ dad ] && isblocked [ cousin ] ) {
+    if ( clip < nt )
+      return 1 ; 
+    if ( isblocked [ lefdes [ clip ] ] && isblocked [ rigdes [ clip ] ] )
+      return 1 ;
+   }  
+  return 0 ;     
+}    
