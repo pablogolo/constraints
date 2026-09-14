3bdb2cd2cf17761c82b514e88b9c9ebd2bbf5f31
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 14:55:56 -0300

    Add no_float_moves_possible function
    Implement no_float_moves_possible function to check conditions for float moves.

--- swapping_constrained_with_floats/no_float_moves_possible.c
+++ swapping_constrained_with_floats/no_float_moves_possible.c
@@ -1,1 +1,7 @@
-
+int no_float_moves_possible( int clip , int dad , int sister ) {
+   if ( grpsiz[ clip ] && isblocked [ dad ] && isblocked [ sister ] ) 
+     if ( grpsiz[ dad ] != grpsiz[ grandpa ] )
+       if ( clip < nt || ( clip > nt && isblocked [ lefdes [ clip ] ] && isblocked [ rigdes [ clip ] ] ) 
+        return 1 ; 
+  return 0 ; 
+}
