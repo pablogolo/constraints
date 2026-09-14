606b2ffaf19d9c0634131065411216e699c517bc
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 14:44:44 -0300

    Add fill_insert_list function implementation
    Implement fill_insert_list function to populate a list.

--- swapping_unconstrained/fill_insert_list.c
+++ swapping_unconstrained/fill_insert_list.c
@@ -1,1 +1,13 @@
-
+int fill_insert_list ( int from , int * list ) {
+  int * todo , * done ;  
+  * ( todo = list ) = from ;
+  done = todo ++ ;
+  while ( done < todo ) {
+    at = * done ++ ; 
+    if ( at >= nt ) { 
+      * todo ++ = lefdes [ at ] ; 
+      * todo ++ = rigdes [ at ] ;
+    }
+  }
+  return todo - list ; 
+}   
