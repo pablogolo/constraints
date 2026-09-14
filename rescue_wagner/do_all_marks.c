32b4a891faaba673e2789280937384e40f1aedd6
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 15:13:36 -0300

    Add do_all_marks function to process taxi marks

--- rescue_wagner/do_all_marks.c
+++ rescue_wagner/do_all_marks.c
@@ -1,1 +1,6 @@
-
+void do_all_marks( char * taxisafloat , char * grpwasok , char * nodeisfix , char * nodtobreak ) {
+  mark_taxisafloat( taxisafloat ) ; 
+  mark_neg_consts_obeyed( grpwasok ) ; 
+  mark_nodes_that_must_be_mono( nodeisfix ) ;
+  mark_nodes_that_are_mono_but_shouldnt_be( nodtobreak ) ; 
+}
