3b8fd1b4469e90dfa3b68339f214b116dfb573b3
Pablo A. Goloboff <pablogolo@yahoo.com.ar>
2026-09-14 15:00:50 -0300

    Implement update_constraints function for tree structure

--- wagner_constrained/update_constraints.c
+++ wagner_constrained/update_constraints.c
@@ -1,1 +1,31 @@
-
+void update_constraints( int add, int position ) { 
+  int a , newnode;
+  int treeconstnum ;
+  treeconstnum = ref_tree [ nt ] ; 
+  newnode = ancestor [ position ] ;
+  if ( position < nt ) {
+    grpsiz[ newnode ] = 0 ;
+    if ( ref_tree [ position ] ) grpsiz[ newnode ] ++ ; 
+    if ( ref_tree [ add ] ) grpsiz[ newnode ] ++ ;
+   }
+  else {
+    grpsiz[ newnode ] = grpsiz[ position ] ;
+    if ( ref_tree [ add ] ) grpsiz[ newnode ] ++ ;
+   }
+  if ( ref_tree [ add ] ) {
+    a = ancestor [ newnode ] ; 
+    while ( a != nt ) {
+      ++ grpsiz[ a ] ;
+      a = ancestor [ a ] ;
+     } 
+    ++ const_nodsiz[ a ] ;
+   }
+  a = ref_tree [ add ];
+  if ( a ) // i.e. a is not a floater... 
+    while ( a < treeconstnum) { 
+      ++ const_nodsiz[ a ] ;
+      constlast [ a ] = add;
+      a = ref_tree [ a ] ;
+    }
+  return ;
+}
